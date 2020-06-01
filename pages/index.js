71c93848e103c3e5ed97e58f824d9f01f2274947
Stef <stef.jones@condenast.com>
2020-06-01 12:27:50 +0100

    Add styling and fix DOM nesting

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,12 @@ export default function Home() {
         <h2 id="software-projects">Software Projects</h2>
         <h3 id="renditions">renditions</h3>
         <p>JavaScript modules for implementing W3C-standard responsive images.</p>
-        <p>
-          Packages:
-          <ul>
-            <li><a href="https://github.com/renditions/get-sizes">@renditions/get-sizes</a></li>
-            <li><a href="https://github.com/renditions/get-srcset">@renditions/get-srcset</a></li>
-            <li><a href="https://github.com/renditions/react-img">@renditions/react-img</a></li>
-          </ul>
-        </p>
+        <p>Packages:</p>
+        <ul>
+          <li><a href="https://github.com/renditions/get-sizes">@renditions/get-sizes</a></li>
+          <li><a href="https://github.com/renditions/get-srcset">@renditions/get-srcset</a></li>
+          <li><a href="https://github.com/renditions/react-img">@renditions/react-img</a></li>
+        </ul>
         <p><a href="https://renditions.dev/project">Project board</a></p>
         <h3 id="letter-press">letter-press</h3>
         <p>Print GitHub Markdown to PDF using headless Chrome. 💌</p>
@@ -38,13 +36,13 @@ export default function Home() {
         <h3 id="content-migrations-at-conde-nast">content migrations at Condé Nast</h3>
         <p>Flyway is a system for migrating content from an existing CMS into Condé Nast's CMS. Content can be defined as articles, authors, categories, galleries, videos and images.</p>
         <p>This system was used to migrate 20 Vogue and GQ websites managed by teams in 10 countries onto a single global web platform managed in London.</p>
-        <p><a href="https://stefee.github.io/flyway-manual/flyway.html">Documentation (mirror)</a></p>
+        <p><a href="https://stefee.github.io/flyway-manual/flyway.html">Flyway Documentation (mirror)</a></p>
         <h3 id="design-systems-at-conde-nast">design systems at Condé Nast</h3>
         <p>&ldquo;A unified, scalable, digital design language for Condé Nast brands.&rdquo;</p>
         <p>Atelier is the React <a href="https://github.com/styled-components/styled-components">styled-components</a> library that feeds Condé Nast's international brand sites, and a living and evolving style guide to be used internally at Condé Nast HQ and by 3rd parties and brand licensees.</p>
-        <p><a href="http://atelier.prod.cni.digital/">Storybook</a></p>
+        <p><a href="http://atelier.prod.cni.digital/">Atelier Storybook</a></p>
         <h3 id="real-time-shock-wave-simulation">real-time shock wave simulation</h3>
-        <p>My research project in the final year of my degree was around simulating shock waves in real time using the Unity game engine. This was broadly considered to be an unsuccessful endeavour.</p>
+        <p>My research project in the final year of my degree was around simulating shock waves in real time using the Unity game engine. This was largely considered to be a failure.</p>
         <h3 id="web-marketing-sites-at-feral-interactive">web marketing sites at Feral Interactive</h3>
         <ul>
           <li><a href="https://www.feralinteractive.com/en/games/lifeisstrange/">Life Is Strange for Mac and Linux</a></li>
@@ -70,6 +68,51 @@ export default function Home() {
 
         * {
           box-sizing: border-box;
+        }
+
+        html {
+          background-color: #ffc0c0;
+        }
+
+        body {
+          padding: 1rem 1rem 4rem;
+          margin: 0 auto;
+          max-width: 42rem;
+        }
+
+        a,
+        a:link,
+        a:visited {
+          color: inherit;
+          text-decoration: none;
+          background-color: #ffaaaa;
+          box-shadow: 0 -3px #ffaaaa, 0 1px #ffaaaa, 0 4px #000;
+        }
+
+        a:focus {
+          outline-width: 4px;
+          outline-style: solid;
+          background-color: #ffdd00;
+          box-shadow: none;
+        }
+
+        p {
+          line-height: 1.6;
+        }
+
+        li {
+          margin-top: 0.9em;
+          margin-bottom: 0.9em;
+        }
+
+        h2 {
+          margin-top: 2em;
+          margin-bottom: 1em;
+        }
+
+        h3 {
+          margin-top: 2em;
+          margin-bottom: 1em;
         }
       `}</style>
     </div>
