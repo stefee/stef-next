0ccd9624e73595006d171a7e52db9958d45a37c4
Stef <stef.jones@condenast.com>
2020-06-01 16:08:07 +0100

    Add emojis and em tags

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ export default function Home() {
         <title>Stef's Website</title>
       </Head>
 
-      <h1>Stef's Website</h1>
+      <h1>Stef's Website 🦖</h1>
 
       <main>
-        <p>Welcome!</p>
-        <h2 id="writing">Writing</h2>
+        <p>Welcome! 👋</p>
+        <h2 id="writing">Writing ✍️</h2>
         <p><a href="https://medium.com/condenastengineering/speedy-builds-with-docker-layer-caching-7ed590ac2fd1">Speedy builds with Docker Layer Caching</a></p>
-        <h2 id="talks">Talks</h2>
+        <h2 id="talks">Talks 🗣</h2>
         <p><a href="https://www.youtube.com/watch?v=AwfXypdYcv0">5 Things I Learned About Software Engineering From Watching Art Restoration Videos</a></p>
-        <h2 id="software-projects">Software Projects</h2>
-        <h3 id="renditions">renditions</h3>
+        <h2 id="software-projects">Software Projects 🌱</h2>
+        <h3 id="renditions"><em>renditions</em></h3>
         <p>JavaScript modules for implementing W3C-standard responsive images.</p>
         <p>Packages:</p>
         <ul>
@@ -25,23 +25,23 @@ export default function Home() {
           <li><a href="https://github.com/renditions/react-img">@renditions/react-img</a></li>
         </ul>
         <p><a href="https://renditions.dev/project">Project board</a></p>
-        <h3 id="letter-press">letter-press</h3>
+        <h3 id="letter-press"><em>letter-press</em></h3>
         <p>Print GitHub Markdown to PDF using headless Chrome. 💌</p>
         <p><a href="https://www.npmjs.com/package/letter-press">Package</a></p>
         <p><a href="https://github.com/stefee/letter-press">Source</a></p>
-        <h3 id="outerspace">outerspace</h3>
+        <h3 id="outerspace"><em>outerspace</em></h3>
         <p>Preserve whitespace surrounding expressions in a template string.</p>
         <p><a href="https://www.npmjs.com/package/outerspace">Package</a></p>
         <p><a href="https://github.com/stefee/outerspace">Source</a></p>
         <h3 id="content-migrations-at-conde-nast">content migrations at Condé Nast</h3>
-        <p>Flyway is a system for migrating content from an existing CMS into Condé Nast's CMS. Content can be defined as articles, authors, categories, galleries, videos and images.</p>
-        <p>This system was used to migrate 20 Vogue and GQ websites managed by teams in 10 countries onto a single global web platform managed in London.</p>
+        <p><em>Flyway</em> is a system for migrating content from an existing CMS into Condé Nast's CMS. Content can be defined as articles, authors, categories, galleries, videos and images.</p>
+        <p>This system was used to migrate 20 <em>Vogue</em> and <em>GQ</em> websites managed by teams in 10 countries onto a single global web platform managed in London.</p>
         <p><a href="https://stefee.github.io/flyway-manual/flyway.html">Flyway Documentation (mirror)</a></p>
         <h3 id="design-systems-at-conde-nast">design systems at Condé Nast</h3>
         <p>&ldquo;A unified, scalable, digital design language for Condé Nast brands.&rdquo;</p>
-        <p>Atelier is the React <a href="https://github.com/styled-components/styled-components">styled-components</a> library that feeds Condé Nast's international brand sites, and a living and evolving style guide to be used internally at Condé Nast HQ and by 3rd parties and brand licensees.</p>
+        <p><em>Atelier</em> is the React <a href="https://github.com/styled-components/styled-components">styled-components</a> library that feeds Condé Nast's international brand sites, and a living and evolving style guide to be used internally at Condé Nast HQ and by 3rd parties and brand licensees.</p>
         <p><a href="http://atelier.prod.cni.digital/">Atelier Storybook</a></p>
-        <h3 id="real-time-shock-wave-simulation">real-time shock wave simulation</h3>
+        <h3 id="real-time-shock-wave-simulation">real-time shock wave simulation 💥</h3>
         <p>My research project in the final year of my degree was around simulating shock waves in real time using the Unity game engine. This was largely considered to be a failure.</p>
         <h3 id="web-marketing-sites-at-feral-interactive">web marketing sites at Feral Interactive</h3>
         <ul>
